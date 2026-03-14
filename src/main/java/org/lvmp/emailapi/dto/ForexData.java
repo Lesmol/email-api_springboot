caae630f9f0188a343c2922521db245bd0cce92f
Lesedi <molemilesedi@gmail.com>
2026-03-14 12:31:51 +0200

    remove unused field

--- src/main/java/org/lvmp/emailapi/dto/ForexData.java
+++ src/main/java/org/lvmp/emailapi/dto/ForexData.java
@@ -2,4 +2,4 @@ package org.lvmp.emailapi.dto;
 
 import java.math.BigDecimal;
 
-public record ForexData(String ticker, BigDecimal open, BigDecimal openingPrice) { }
+public record ForexData(String ticker, BigDecimal openingPrice) { }
