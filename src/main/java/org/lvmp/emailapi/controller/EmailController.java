cf578b00c6fdbc8f677cc5a77156f11d0c919582
Lesedi <molemilesedi@gmail.com>
2026-03-14 11:52:44 +0200

    remove unused imports

--- src/main/java/org/lvmp/emailapi/controller/EmailController.java
+++ src/main/java/org/lvmp/emailapi/controller/EmailController.java
@@ -1,19 +1,12 @@
 package org.lvmp.emailapi.controller;
 
-import jakarta.validation.constraints.Email;
-import org.lvmp.emailapi.dto.ForexData;
 import org.lvmp.emailapi.dto.MarketUpdate;
-import org.lvmp.emailapi.dto.StockData;
 import org.lvmp.emailapi.service.EmailService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1")
